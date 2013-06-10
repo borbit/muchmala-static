a4c6ae8fd8e8ba31e5cb0fa13f7b579385c2b395
Serge Borbit <serge.borbit@gmail.com>
2013-06-10 10:38:26 +0300

    Next/prev buttons

--- public/comp/panel/panel.js
+++ public/comp/panel/panel.js
@@ -8,6 +8,7 @@
       'click .panel__btn_about': 'showAbout'
     , 'click .panel__btn_boards': 'showBoards'
     , 'click .panel__btn_profile': 'showProfile'
+    , 'click .panel__btn_prev': 'prevPuzzle'
     , 'click .panel__btn_next': 'nextPuzzle'
     , 'click .panel__info': 'switchBoard'
     },
@@ -17,21 +18,17 @@
     initialize: function(op) {
       this.game = op.game;
       this.user = op.game.user;
+      this.puzzle = op.game.puzzle;
 
       this.ui = {
         info: this.$el.find('.panel__info')
+      , btnPrev: this.$el.find('.panel__btn_prev')
       , btnNext: this.$el.find('.panel__btn_next')
       , btnBoards: this.$el.find('.panel__btn_boards')
       };
 
       this.render();
       this.listenTo(this.user, 'change', this.render);
-      this.listenTo(this.game, 'puzzle:loading', function() {
-        this.btnLoading(this.ui.btnNext);
-      });
-      this.listenTo(this.game, 'puzzle:load', function() {
-        this.btnReset(this.ui.btnNext);
-      });
       this.listenToOnce(this.game, 'puzzle:load', function() {
         this.show();
       });
@@ -79,13 +76,33 @@
       this.render();
     },
 
+    prevPuzzle: function() {
+      var self = this;
+      var url = sprintf('%s/prev/%s', API_URL, this.puzzle.get('id'));
+      
+      this.btnLoading(this.ui.btnPrev);
+      this.listenToOnce(this.game, 'puzzle:load', function() {
+        self.btnReset(self.ui.btnPrev);
+      });
+
+      $.get(url, function(puzzleId) {
+        Backbone.history.navigate('puzzle/' + puzzleId);
+        self.game.leftCurrentPuzzle();
+        self.game.getPuzzle(puzzleId);
+      });
+    },
+
     nextPuzzle: function() {
       var self = this;
-      this.btnLoading(this.ui.btnNext);
+      var url = sprintf('%s/next/%s', API_URL, this.puzzle.get('id'));
 
-      $.get(sprintf('%s/rand', API_URL), function(puzzleId) {
-        Backbone.history.navigate('puzzle/' + puzzleId);
+      this.btnLoading(this.ui.btnNext);
+      this.listenToOnce(this.game, 'puzzle:load', function() {
         self.btnReset(self.ui.btnNext);
+      });
+
+      $.get(url, function(puzzleId) {
+        Backbone.history.navigate('puzzle/' + puzzleId);
         self.game.leftCurrentPuzzle();
         self.game.getPuzzle(puzzleId);
       });
