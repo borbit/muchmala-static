e7f3323730f4f4072b7732d7ed0ad46f093e96e2
George Miroshnykov <george.miroshnykov@gmail.com>
2013-06-16 16:47:14 +0300

    Updated muchmala-static for automated provisioning

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var path = require('path');
 
 module.exports = {
-  host: process.env.STATIC_HOST || '0.0.0.0'
-, port: process.env.STATIC_PORT || '8002'
+  host: '0.0.0.0'
+, port: process.env.PORT || '8002'
 
 , ioUrl     : process.env.IO_URL     || 'http://localhost:8001'
 , apiUrl    : process.env.API_URL    || 'http://localhost:8003'
